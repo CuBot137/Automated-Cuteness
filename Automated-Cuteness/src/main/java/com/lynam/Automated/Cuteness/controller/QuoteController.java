b16f1697a21aae20a7d014126d9dc8522e569e78
conor <conorlynam1234@gmail.com>
2023-11-24 18:19:36 +0000

    Service and Controller

---
+++ Automated-Cuteness/src/main/java/com/lynam/Automated/Cuteness/controller/QuoteController.java
@@ -0,0 +1,7 @@
+package com.lynam.Automated.Cuteness.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QuoteController {
+}
---
+++ Automated-Cuteness/src/main/java/com/lynam/Automated/Cuteness/service/QuoteService.java
@@ -0,0 +1,7 @@
+package com.lynam.Automated.Cuteness.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class QuoteService {
+}
