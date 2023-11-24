0467783ef9672c9ebde8077c85594e2a415820db
conor <conorlynam1234@gmail.com>
2023-11-24 18:08:53 +0000

    First commit

---
+++ Automated-Cuteness/src/main/java/com/lynam/Automated/Cuteness/AutomatedCutenessApplication.java
@@ -0,0 +1,13 @@
+package com.lynam.Automated.Cuteness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedCutenessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedCutenessApplication.class, args);
+	}
+
+}
---
+++ Automated-Cuteness/src/test/java/com/lynam/Automated/Cuteness/AutomatedCutenessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lynam.Automated.Cuteness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedCutenessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
