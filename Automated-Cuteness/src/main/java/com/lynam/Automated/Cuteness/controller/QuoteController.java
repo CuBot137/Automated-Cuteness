3dc57ee98cf64f9b0e9190029e88a04437cfbc86
conor <conorlynam1234@gmail.com>
2023-11-27 17:11:58 +0000

    Quote is now generated

--- Automated-Cuteness/src/main/java/com/lynam/Automated/Cuteness/controller/QuoteController.java
+++ Automated-Cuteness/src/main/java/com/lynam/Automated/Cuteness/controller/QuoteController.java
@@ -1,5 +1,8 @@
 package com.lynam.Automated.Cuteness.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lynam.Automated.Cuteness.externalApi.QuoteApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +15,12 @@ public class QuoteController {
     private QuoteApi quoteApi;
 
     @GetMapping("/quote")
-    public String getQuote(){
+    public String getQuote() throws JsonProcessingException {
         String apiResponse = quoteApi.callQuoteApi();
         System.out.println(apiResponse);
-        return apiResponse;
+
+        ObjectMapper mapper = new ObjectMapper();
+//        JsonNode jsonNode = mapper.readTree(apiResponse);
+        return mapper.readTree(apiResponse).findValue("content").asText();
     }
 }
--- Automated-Cuteness/src/main/java/com/lynam/Automated/Cuteness/externalApi/QuoteApi.java
+++ Automated-Cuteness/src/main/java/com/lynam/Automated/Cuteness/externalApi/QuoteApi.java
@@ -1,14 +1,19 @@
 package com.lynam.Automated.Cuteness.externalApi;
 
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+@Component
 public class QuoteApi {
 
     public String callQuoteApi(){
         String uri = "https://api.quotable.io/random";
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(uri, String.class);
+//        Gson gson = new GsonBuilder().setPrettyPrinting().create();
         return result;
     }
 }
