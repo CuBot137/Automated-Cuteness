b48fd5770537344f346f0fa4c8c52e8e5d7afdba
conor <conorlynam1234@gmail.com>
2024-01-05 14:28:02 +0000

    Added prod and dev profiles

--- Automated-Cuteness/src/main/java/com/lynam/Automated/Cuteness/config/ProjectConfig.java
+++ Automated-Cuteness/src/main/java/com/lynam/Automated/Cuteness/config/ProjectConfig.java
@@ -9,4 +9,5 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = "com/lynam/Automated/Cuteness")
 public class ProjectConfig {
 
+
 }
--- Automated-Cuteness/src/main/java/com/lynam/Automated/Cuteness/service/QuoteService.java
+++ Automated-Cuteness/src/main/java/com/lynam/Automated/Cuteness/service/QuoteService.java
@@ -9,9 +9,7 @@ import com.twilio.Twilio;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
 import java.sql.SQLException;
 
 
