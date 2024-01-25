bef64087887e87598bbc9a5b4a4471114fff6a22
conor <conorlynam1234@gmail.com>
2024-01-25 16:46:55 +0000

    It is finally hosted on render and connected to the remote databasegit add .!

--- Automated-Cuteness/src/main/java/com/lynam/Automated/Cuteness/AutomatedCutenessApplication.java
+++ Automated-Cuteness/src/main/java/com/lynam/Automated/Cuteness/AutomatedCutenessApplication.java
@@ -4,6 +4,7 @@ package com.lynam.Automated.Cuteness;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
