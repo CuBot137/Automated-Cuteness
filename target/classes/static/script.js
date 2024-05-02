aeaf250fe9958696fc090d1c7587c41c79543c8b
conor <conorlynam1234@gmail.com>
2024-05-02 15:04:21 +0100

    remmoved jar from .gitignore

---
+++ target/classes/static/script.js
@@ -0,0 +1,6 @@
+function revealText() {
+    var hiddenText = document.getElementById("hiddenText");
+
+    // Toggle the visibility of the text
+    hiddenText.style.display = (hiddenText.style.display === "none") ? "block" : "none";
+}
